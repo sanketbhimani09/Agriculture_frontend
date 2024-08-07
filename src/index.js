7afb17066fa0269549a246813745d173efab8a79
Yash Vinzuda <vinzuday@gmail.com>
2024-08-07 22:05:55 +0530

    Testing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-    <h1>Hello Sanket from yash</h1>
+    <h1>Hello Sanket from yash...</h1>
     </>
 );
 
